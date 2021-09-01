d55b9a4563a0c4fc26ae80ca8b81fcab91997bea
Aksel <axel.akonde@gmail.com>
2021-09-01 02:01:00 +0200

    requete affichage posts des utilisateurs suivis

--- LITReview/reviews/views.py
+++ LITReview/reviews/views.py
@@ -5,6 +5,8 @@ from django.views.generic import TemplateView
 from core.custom_decorators import custom_login_required
 
 from reviews.models import Ticket
+from subscriptions.models import UserFollows
+from core.models import CustomUser
 
 
 class PostListsView(TemplateView):  #  faire une seule classe au final ! (fusionner, factoriser tout ce qui est "posts"
@@ -20,14 +22,33 @@ class PostListsView(TemplateView):  #  faire une seule classe au final ! (fusio
         """
 
         """
+        self.context = {}
         url_name = self.add_url_name_to_context(request)
 
         if url_name == 'feed':
             self.context['title'] = "Page d'accueil - Flux"
-            # posts_for_following_users_query = Ticket.objects.filter(user_id__in=request.user.).values()
-            # (en cours : lister les posts (juste les tickets pour le moment) des user que l'utilisateur suit (pour la page feed))
 
-        elif url_name == 'posts':
+            following_users_posts = Ticket.objects.select_related('user_follows', 'custom_user').values()
+            # (en cours : lister les posts (juste les tickets pour le moment) des user que l'utilisateur suit (pour la page feed))
+            #mauvaise requete je ne sais pas ce que je ramene a retravailler !
+
+            """
+            user_follows_query = UserFollows.objects.filter(user_id=request.user.id).values()
+            user_follows_id_list = [UserFollows.objects.filter(user_id=user_follows_dict['followed_user_id'])
+                                    for user_follows_dict in user_follows_query]
+
+            followed_users_list = [CustomUser.objects.filter(id__in=user_id)
+                                   for user_id in user_follows_id_list]
+
+            followed_users = [CustomUser.objects.filter(id=followed_users_dict['id'])
+                              for followed_users_dict in followed_users_list]
+            followed_users_posts_query = Ticket.objects.filter(user__in=followed_users).values()
+            following_users_posts = [Ticket.objects.filter(user_id=followed_users)
+                                     for followed_users in followed_users_posts_query]
+            """
+            self.context['following_users_posts'] = following_users_posts
+
+        if url_name == 'posts':
             self.context['title'] = "Mes posts"
 
             user_posts_query = Ticket.objects.filter(user_id=request.user.id).values()    # ajouter les reviews aussi plus tard
