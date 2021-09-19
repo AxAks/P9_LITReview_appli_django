139db9704a0437017776b993aedfc708505541dc
Aksel <axel.akonde@gmail.com>
2021-09-20 00:00:20 +0200

    resolution bug plain text ! (debut): le souci venait des views.py pb render avec form ET context ne passait pas fait sur tiket création par contre enregistrer d'un ticket en base ne fonctionne pas à cause de la not null constrainte sur l'id

--- LITReview/reviews/forms.py
+++ LITReview/reviews/forms.py
@@ -19,21 +19,12 @@ from reviews.models import Ticket, Review
 class TicketCreationForm(forms.ModelForm):
     title = forms.CharField(label="titre", max_length=128, help_text='Titre')
     description = forms.CharField(label="description", max_length=2048, help_text='Description')
-    image = forms.ImageField(label="image", help_text='Image')
+    image = forms.ImageField(label="image", help_text='Image', required=False)
 
     class Meta:
         model = Ticket
         fields = ('title', 'description',
                   'image',)
-
-    """
-    title = models.CharField(max_length=128)
-    description = models.TextField(max_length=2048, blank=True)
-    user = models.ForeignKey(
-        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='ticket_images', null=True, blank=True)
-    time_created = models.DateTimeField(auto_now_add=True)
-    """
 
 
 class ReviewCreationForm(forms.ModelForm):
@@ -45,15 +36,3 @@ class ReviewCreationForm(forms.ModelForm):
         model = Review
         fields = ('headline', 'rating',
                   'body',)
-
-    """
-    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField(
-        # validates that rating must be between 0 and 5
-        validators=[MinValueValidator(0), MaxValueValidator(5)])
-    user = models.ForeignKey(
-        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    headline = models.CharField(max_length=128)
-    body = models.CharField(max_length=8192, blank=True)
-    time_created = models.DateTimeField(auto_now_add=True)
-    """
