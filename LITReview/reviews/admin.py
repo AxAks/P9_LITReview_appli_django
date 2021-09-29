04f9c03fa347d6ad812f02b8c3803ab958eb2c34
Aksel <axel.akonde@gmail.com>
2021-09-29 17:36:30 +0200

    ajout models des apps dans admin.py's pour gestion admin

--- LITReview/reviews/admin.py
+++ LITReview/reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Ticket, Review
+
+admin.site.register(Ticket)
+admin.site.register(Review)
