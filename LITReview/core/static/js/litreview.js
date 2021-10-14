0826488d65c3da7e17cd8ce734d0632c60baadd2
Aksel <axel.akonde@gmail.com>
2021-10-14 23:34:32 +0200

    JS Notes Reviews stars + début CSS Flex à continuer

--- LITReview/core/static/js/litreview.js
+++ LITReview/core/static/js/litreview.js
@@ -19,26 +19,6 @@ function addStarsForRating() {
     }
 }
 
-/*
-function addStarsForRating() {
-    let reviews_ratings = Array.from(document.getElementsByClassName("rating")).map(element => parseInt(element.innerHTML));
-    let star_ratings = Array.from(document.getElementsByClassName("star-rating"));
-    for (review_rating of reviews_ratings) {
-        for (star_rating of star_ratings) {
-            for (possible_rating of possible_ratings) {
-                if (review_rating == 0) {
-                    (star_rating.innerHTML += empty_star) * 5;
-                } else if (possible_rating <= review_rating) {
-                    star_rating.innerHTML += filled_star;
-                } else {
-                    star_rating.innerHTML += empty_star;
-                };
-            };
-        };
-    };
-};
-*/
-// Launch functions when DOM is ready
 window.onload = () => {
     addStarsForRating();
 };
